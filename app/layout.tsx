0dc63e6aa37d8aa4de3cf42098d8a5a2e268119e
PushToTalkMe <vladvladilin@mail.ru>
2024-06-18 17:53:53 +0300

    feat: add orval, api-instance and home page fsd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/app/globals.css";
 import { Inter as FontSans } from "next/font/google";
-import { cn } from "@/lib/utils";
+import { cn } from "@/shared/lib/utils";
 
 const fontSans = FontSans({
   subsets: ["latin"],
---
+++ src/app/app-provider.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+
+export function AppProvider({ children }: { children: any }): JSX.Element {
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
